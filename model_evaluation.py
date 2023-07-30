42ed1b645aba704133db3ffa27b2c68c7740b3fa
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-31 09:44:06 +1000

    basic model_evaluation.py Accuracy: This is the ratio of the total number of correct predictions to the total number of predictions. In other words, it is the proportion of true results (both true positives and true negatives) among the total number of cases examined. However, it can be a misleading measure if the classes are imbalanced.
    Precision: This is the ratio of true positives (predicted as positive and actually positive) to the total number of predicted positives (both true and false positives). It's a measure of how many of the predicted positive instances are actually positive.
    
    Recall (or Sensitivity or True Positive Rate): This is the ratio of true positives to the total number of actual positives (both predicted correctly and incorrectly). It measures how many of the actual positive instances the model correctly identified.
    
    F1 Score: The F1 Score is the harmonic mean of precision and recall. It is a balanced measure for checking precision and recall for uneven class distribution.
    
    AUC-ROC (Area Under the Receiver Operating Characteristics Curve): The ROC curve is a plot with the True Positive Rate (TPR) on the y-axis and the False Positive Rate (FPR) on the x-axis. The area under this curve (AUC) is used as a metric to differentiate between classes. The higher the AUC, the better the model is at distinguishing between positive and negative classes. An AUC of 0.5 signifies a model that is no better than random, while an AUC of 1 signifies a perfect model.
    
    TPR (True Positive Rate): Also known as recall or sensitivity, it measures the proportion of positives that are correctly identified.
    
    FPR (False Positive Rate): It measures the proportion of negatives that are incorrectly identified as positive.

---
+++ model_evaluation.py
@@ -0,0 +1,68 @@
+import joblib
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def get_analytics():
+    # Load saved model
+
+    model = joblib.load('models/phishing_detection_model.pkl')
+
+    # Load data
+
+    combined_data = pd.read_csv("data/5.urldata.csv")
+
+    # Prepare data
+
+    x = combined_data.drop(['Domain', 'Label'], axis=1)
+    y = combined_data['Label']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    # Predict target values
+
+    y_pred = model.predict(x_test)
+
+    # Calculate Metrics
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    # Print Metrics
+
+    print("Multilayer Perceptrons: Accuracy: {:.3f}".format(accuracy))
+    print("Multilayer Perceptrons: Precision: {:.3f}".format(precision))
+    print("Multilayer Perceptrons: Recall: {:.3f}".format(recall))
+    print("Multilayer Perceptrons: F1: {:.3f}".format(f1))
+
+    # Calculate AUC-ROC
+
+    auc = roc_auc_score(y_test, y_pred)
+
+    # Print AUC-ROC
+
+    print("Multilayer Perceptrons: AUC-ROC: {:.3f}".format(auc))
+
+    # Generate ROC curve values for fpr, tpr, thresholds
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+
+
+    # Plot ROC curve
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+if __name__ == "__main__":
+    get_analytics()
