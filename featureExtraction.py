7d7026e08c6402e37d485f80f4c2634e2e5f9365
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-31 09:58:43 +1000

    written features to extract

---
+++ featureExtraction.py
@@ -0,0 +1,8 @@
+# Functions We Need
+# Domain, IP, @, URL Length, URL Depth, Redirect (//_), https,
+# url shortening, prefix/suffix (using - or @ in domain name),
+# DNS records (if avail), Website Traffic (if avail, ranking),
+# Domain Age, Domain Expiry, iFrame, (If domain uses iFrame in url webpage)
+# Mouse Over, Right click, Web Forwards
+
+# We dropped domain in training data
