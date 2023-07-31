4a295a514ff6d97c7cf99b336ba16e089d3e1731
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-31 10:18:45 +1000

    finished featureExtraction.py
    exporting features as a df

--- featureExtraction.py
+++ featureExtraction.py
@@ -4,8 +4,15 @@
 # DNS records (if avail), Website Traffic (if avail, ranking),
 # Domain Age, Domain Expiry, iFrame, (If domain uses iFrame in url webpage)
 # Mouse Over, Right click, Web Forwards
+from datetime import datetime
 import ipaddress
+import urllib
+import urllib.request
+import whois
 from urllib.parse import urlparse
+import re
+from bs4 import BeautifulSoup
+import requests
 
 
 # We dropped domain in training data
@@ -20,6 +27,7 @@ def havingIP(url):
         ip = 0
     return ip
 
+
 # @ sign
 def haveAtSign(url):
     if '@' in url:
@@ -27,6 +35,7 @@ def haveAtSign(url):
     else:
         at = 0
     return at
+
 
 # URL length if longer than 54 characters mark as phishing
 
@@ -36,6 +45,7 @@ def urlLength(url):
     else:
         length = 1
     return length
+
 
 # Return URL depth
 
@@ -47,16 +57,18 @@ def urlDepth(url):
             depth = depth + 1
     return depth
 
+
 # Checking number of redirections, if greater than 6 mark as phishing
 def redirection(url):
-  pos = url.rfind('//')
-  if pos > 6:
-    if pos > 7:
-      return 1
+    pos = url.rfind('//')
+    if pos > 6:
+        if pos > 7:
+            return 1
+        else:
+            return 0
     else:
-      return 0
-  else:
-    return 0
+        return 0
+
 
 # Check if https is in the url
 def httpsInUrl(url):
@@ -66,4 +78,199 @@ def httpsInUrl(url):
     else:
         return 0
 
-#
+
+# Check if url is shortened
+
+def urlShort(url):
+    shortening_services = r"bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|" \
+                          r"yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|" \
+                          r"short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|" \
+                          r"doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|db\.tt|" \
+                          r"qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|q\.gs|is\.gd|" \
+                          r"po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|x\.co|" \
+                          r"prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|" \
+                          r"tr\.im|link\.zip\.net"
+    match = re.search(shortening_services, url)
+    if match:
+        return 1
+    else:
+        return 0
+
+
+# Check if prefix or suffix is used in domain name
+
+def prefixSuffix(url):
+    if '-' in urlparse(url).netloc:
+        return 1
+    else:
+        return 0
+
+
+# website traffic
+
+# 12.Web traffic (Web_Traffic)
+def web_traffic(url):
+    try:
+        # Filling the whitespaces in the URL if any
+        url = urllib.parse.quote(url)
+        rank = \
+        BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(), "xml").find(
+            "REACH")['RANK']
+        rank = int(rank)
+    except TypeError:
+        return 1
+    if rank < 100000:
+        return 1
+    else:
+        return 0
+
+
+# domain age
+
+
+def domainAge(domain_name):
+    creation_date = domain_name.creation_date
+    expiration_date = domain_name.expiration_date
+    if (isinstance(creation_date, str) or isinstance(expiration_date, str)):
+        try:
+            creation_date = datetime.strptime(creation_date, '%Y-%m-%d')
+            expiration_date = datetime.strptime(expiration_date, "%Y-%m-%d")
+        except:
+            return 1
+    if ((expiration_date is None) or (creation_date is None)):
+        return 1
+    elif ((type(expiration_date) is list) or (type(creation_date) is list)):
+        return 1
+    else:
+        ageofdomain = abs((expiration_date - creation_date).days)
+        if ((ageofdomain / 30) < 6):
+            age = 1
+        else:
+            age = 0
+    return age
+
+
+# domain expiry
+
+def domainExpiry(domain_name):
+    expiration_date = domain_name.expiration_date
+    if isinstance(expiration_date, str):
+        try:
+            expiration_date = datetime.strptime(expiration_date, "%Y-%m-%d")
+        except:
+            return 1
+    if (expiration_date is None):
+        return 1
+    elif (type(expiration_date) is list):
+        return 1
+    else:
+        today = datetime.now()
+        end = abs((expiration_date - today).days)
+        if ((end / 30) < 6):
+            end = 0
+        else:
+            end = 1
+    return end
+
+
+# iFrame
+
+def iframe(response):
+    if response == "":
+        return 1
+    else:
+        if re.findall(r"[<iframe>|<frameBorder>]", response.text):
+            return 1
+        else:
+            return 0
+
+
+# Mouse Over
+
+def mouseOver(response):
+    if response == "":
+        return 1
+    else:
+        if re.findall("<script>.+onmouseover.+</script>", response.text):
+            return 1
+        else:
+            return 0
+
+
+# Right Click (disabled)
+
+def rightClick(response):
+    if response == "":
+        return 1
+    else:
+        if re.findall(r"event.button ?== ?2", response.text):
+            return 1
+        else:
+            return 0
+
+
+# Check how many times a website has been redirected
+
+def forwarding(response):
+    if response == "":
+        return 1
+    else:
+        if len(response.history) <= 2:
+            return 0
+        elif len(response.history) <= 4:
+            return 1
+        else:
+            return 0
+
+
+# Computing URL features
+
+def featureExtraction(url):
+    features = []
+
+    features.append(havingIP(url))
+    features.append(haveAtSign(url))
+    features.append(urlLength(url))
+    features.append(urlDepth(url))
+    features.append(redirection(url))
+    features.append(httpsInUrl(url))
+    features.append(urlShort(url))
+    features.append(prefixSuffix(url))
+
+    # Domain based features (Checking DNS Record)
+
+    dns = 0
+    try:
+        domain_name = whois.whois(urlparse(url).netloc)
+    except:
+        dns = 1
+
+    features.append(dns)
+    features.append(web_traffic(url))
+    features.append(1 if dns == 1 else domainAge(domain_name))
+    features.append(1 if dns == 1 else domainExpiry(domain_name))
+
+    # HTML & Javascript based features
+
+    try:
+        response = requests.get(url)
+    except:
+        response = ""
+
+    features.append(iframe(response))
+    features.append(mouseOver(response))
+    features.append(rightClick(response))
+    features.append(forwarding(response))
+
+    return features
+
+
+# Convert features to dataframe
+
+def featureExtractionDF(url):
+    featureExtraction(url)
+    featureDF = ['Domain', 'Have_IP', 'Have_At', 'URL_Length', 'URL_Depth', 'Redirection',
+                 'https_Domain', 'TinyURL', 'Prefix/Suffix', 'DNS_Record', 'Web_Traffic',
+                 'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over', 'Right_Click', 'Web_Forwards', 'Label']
+
+    return featureDF
